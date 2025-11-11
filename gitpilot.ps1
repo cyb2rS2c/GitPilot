47fba41864a69d36beebf1dc445961f04488d6e4
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-11 12:39:25 +0100

    Add PowerShell script to set up GitPilot Automatically

---
+++ gitpilot.ps1
@@ -0,0 +1,7 @@
+﻿curl -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
+Expand-Archive -Force  .\GitPilot-main.zip
+cd GitPilot-main/GitPilot-main
+python -m venv myvenv
+./myvenv/Scripts/Activate.ps1
+pip install -r requirements.txt
+python GitPilot.py
