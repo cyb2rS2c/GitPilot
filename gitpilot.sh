27a5029fc8f9b4a175db98f77abcc41757285b30
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-11 12:50:08 +0100

    Add script to download and run GitPilot

---
+++ gitpilot.sh
@@ -0,0 +1,7 @@
+curl -L -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
+unzip -o GitPilot-main.zip
+cd GitPilot-main
+python3 -m venv myvenv
+source myvenv/bin/activate
+pip install -r requirements.txt
+python3 gitPilot.py
