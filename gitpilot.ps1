b3ee489088c684c6d26f5048557dc014c3c91624
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-12 09:46:58 +0100

    Delete gitpilot.ps1

--- gitpilot.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
-curl -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
-Expand-Archive -Force  .\GitPilot-main.zip
-cd GitPilot-main/GitPilot-main
-python -m venv myvenv
-./myvenv/Scripts/Activate.ps1
-pip install -r requirements.txt
-Write-Host "Please execute the following command: 'python gitpilot.py' using a python interpreter to avoid any errors"
