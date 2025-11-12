acfdc4e6646696874c4d114d5ded62561933e954
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-12 09:45:26 +0100

    Update gitpilot.bat to download and set up GitPilot
    Replaced script execution with setup for GitPilot.

--- gitpilot.bat
+++ gitpilot.bat
@@ -1,5 +1,9 @@
 @echo off
-REM launches the ps1 script with an interactive console window
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0gitpilot.ps1"
-
-pause
+curl -L -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
+powershell -Command "Expand-Archive -Force .\GitPilot-main.zip"
+cd GitPilot-main\GitPilot-main
+python -m venv myvenv
+call myvenv\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+cmd /k "myvenv\Scripts\python.exe -i -c ""exec(open('gitpilot.py').read())"""
