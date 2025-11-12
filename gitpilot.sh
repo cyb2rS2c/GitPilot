e0c9ce8f6a038692645e2afc6623a3debfbfa23a
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-12 13:34:49 -0500

    Fix directory change for GitPilot setup script

--- gitpilot.sh
+++ gitpilot.sh
@@ -2,7 +2,7 @@
 set -e
 curl -L -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
 unzip -o GitPilot-main.zip
-cd GitPilot-main/GitPilot-main
+cd GitPilot-main
 python3 -m venv myvenv
 source myvenv/bin/activate
 python3 -m pip install --upgrade pip
