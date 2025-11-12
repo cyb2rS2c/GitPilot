11843277a8dcc6c50effaf6f6987a2ff1eb609a5
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-11-12 09:46:44 +0100

    Add PowerShell command to set execution policy

--- gitpilot.bat
+++ gitpilot.bat
@@ -1,5 +1,6 @@
 @echo off
 curl -L -o GitPilot-main.zip https://github.com/cyb2rS2c/GitPilot/archive/refs/heads/main.zip
+powershell -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
 powershell -Command "Expand-Archive -Force .\GitPilot-main.zip"
 cd GitPilot-main\GitPilot-main
 python -m venv myvenv
